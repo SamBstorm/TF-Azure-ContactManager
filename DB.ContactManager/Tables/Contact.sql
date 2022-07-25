aab6f9ea9bf7ac2fd7902b08d059700c6c34cf00
Legrain Samuel <samuel.legrain@bstorm.be>
2022-07-25 10:37:39 +0200

    Update DB

--- DB.ContactManager/Tables/Contact.sql
+++ DB.ContactManager/Tables/Contact.sql
@@ -9,8 +9,9 @@
 	[UtilisateurId] INT NOT NULL,
 	[CategorieId] INT NOT NULL,
 	CONSTRAINT PK_Contact PRIMARY KEY ([Id]),
-	CONSTRAINT UK_Contact_Email UNIQUE ([Email]),
-	CONSTRAINT UK_Contact_Tel UNIQUE ([Tel]),
+	CONSTRAINT UK_Contact_Email UNIQUE ([Email],[UtilisateurId]),
+	CONSTRAINT UK_Contact_Tel UNIQUE ([Tel],[UtilisateurId]),
+	CONSTRAINT CK_Contact_Email_Tel CHECK ([Email] IS NOT NULL OR [Tel] IS NOT NULL),
 	CONSTRAINT FK_Contact_Utilisateur FOREIGN KEY ([UtilisateurId]) REFERENCES [Utilisateur]([Id]),
-	CONSTRAINT FK_Contact_Categorie FOREIGN KEY ([CategorieId]) REFERENCES [Categorie]([Id]),
+	CONSTRAINT FK_Contact_Categorie FOREIGN KEY ([CategorieId]) REFERENCES [Categorie]([Id])
 )
