b041fba725556ba6a79dc726c33606d228dab401
Legrain Samuel <samuel.legrain@bstorm.be>
2022-07-27 09:09:28 +0200

    Add Model for Auth\Login form with validators

--- ASP.ContactManager/Controllers/AuthController.cs
+++ ASP.ContactManager/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.RegularExpressions;
 
 namespace ASP.ContactManager.Controllers
 {
@@ -9,19 +12,37 @@ namespace ASP.ContactManager.Controllers
             return RedirectToAction("login");
         }
 
-        [HttpGet]
+
+        //GET par defaut pour toute action
+        //Login() charge la vue du formulaire
         public IActionResult Login()
         {
             return View();
         }
 
+        //POST est défini pour la récupération de données
         [HttpPost]
-        public IActionResult Login(IFormCollection form)
+        //Login(IFormCollection form) récupère les données du formulaire dans form
+        //Login(string email, string password) récupère chacun des input dans des paramètres distincts
+        public IActionResult Login(AuthLoginForm form)
         {
+            ValidateAuthLoginForm(form, ModelState);
+            if (ModelState.IsValid) return RedirectToAction("Index", "Home");
             return View();
         }
 
-        [HttpGet]
+        private void ValidateAuthLoginForm(AuthLoginForm form, ModelStateDictionary modelState)
+        {
+            if (string.IsNullOrWhiteSpace(form.Email))
+                modelState.AddModelError(nameof(form.Email), "L'email est obligatoire.");
+
+            if (string.IsNullOrWhiteSpace(form.Password))
+                modelState.AddModelError(nameof(form.Password), "Le mot de passe est obligatoire.");
+
+            if (form.Password != null && !Regex.IsMatch(form.Password, @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&\-+=()]).{8,32}$"))
+                modelState.AddModelError(nameof(form.Password), "Le mot de passe ne correspond pas à la norme...");
+        }
+
         public IActionResult Register()
         {
             return View();
---
+++ ASP.ContactManager/Models/AuthLoginForm.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.ContactManager.Models
+{
+    public class AuthLoginForm
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
